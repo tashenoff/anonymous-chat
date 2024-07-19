3139396c1a4735ef4df23773dc005e29f3e42cb8
tashenoff <kail.dead.kd@gmail.com>
2024-07-19 17:59:13 +0600

    .

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -5,7 +5,7 @@ const UserForm = ({ onSearch }) => {
     const [ageRange, setAgeRange] = useState('');
     const [partnerGender, setPartnerGender] = useState('');
     const [partnerAgeRange, setPartnerAgeRange] = useState('');
-    const [selectedInterest, setSelectedInterest] = useState(''); // Инициализация состояния для интересов
+    const [selectedInterest, setSelectedInterest] = useState('');
     const [error, setError] = useState('');
 
     const handleSubmit = (e) => {
@@ -43,79 +43,106 @@ const UserForm = ({ onSearch }) => {
 
     return (
         <div className='mx-5 h-full flex flex-col items-center justify-center'>
-            <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-white shadow-md rounded-lg grid gap-4 p-8">
+            <form onSubmit={handleSubmit} className="max-w-4xl mx-auto bg-white shadow-md rounded-lg p-8 grid grid-cols-1 md:grid-cols-2 gap-6">
                 {error && <div className="mb-4 text-red-500 col-span-2">{error}</div>}
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="gender">Ваш пол:</label>
-                    <select
-                        id="gender"
-                        value={gender}
-                        onChange={(e) => setGender(e.target.value)}
-                        className="w-full p-2 border border-gray-300 rounded-md"
-                    >
-                        <option value="">Выберите пол</option>
-                        <option value="male">Мужской</option>
-                        <option value="female">Женский</option>
-                    </select>
+                
+                {/* Пользователь */}
+                <div className="flex flex-col gap-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">Ваш пол:</label>
+                        <div className="flex flex-wrap gap-2">
+                            <button
+                                type="button"
+                                onClick={() => setGender('male')}
+                                className={`w-full py-2 px-4 rounded-md ${gender === 'male' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+                            >
+                                Мужской
+                            </button>
+                            <button
+                                type="button"
+                                onClick={() => setGender('female')}
+                                className={`w-full py-2 px-4 rounded-md ${gender === 'female' ? 'bg-pink-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+                            >
+                                Женский
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">Ваш возраст:</label>
+                        <div className="flex flex-wrap gap-2">
+                            {ageRanges.map((range) => (
+                                <button
+                                    key={range.value}
+                                    type="button"
+                                    onClick={() => setAgeRange(range.value)}
+                                    className={`w-full py-2 px-4 rounded-md ${ageRange === range.value ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+                                >
+                                    {range.label}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">Ваши интересы:</label>
+                        <div className="flex flex-wrap gap-2">
+                            {interestsOptions.map((interest) => (
+                                <button
+                                    key={interest}
+                                    type="button"
+                                    onClick={() => setSelectedInterest(interest)}
+                                    className={`w-full py-2 px-4 rounded-md ${selectedInterest === interest ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+                                >
+                                    {interest}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ageRange">Ваш возраст:</label>
-                    <select
-                        id="ageRange"
-                        value={ageRange}
-                        onChange={(e) => setAgeRange(e.target.value)}
-                        className="w-full p-2 border border-gray-300 rounded-md"
-                    >
-                        <option value="">Выберите возраст</option>
-                        {ageRanges.map((range) => (
-                            <option key={range.value} value={range.value}>{range.label}</option>
-                        ))}
-                    </select>
+
+                {/* Собеседник */}
+                <div className="flex flex-col gap-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">Пол партнера:</label>
+                        <div className="flex flex-wrap gap-2">
+                            <button
+                                type="button"
+                                onClick={() => setPartnerGender('male')}
+                                className={`w-full py-2 px-4 rounded-md ${partnerGender === 'male' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+                            >
+                                Мужской
+                            </button>
+                            <button
+                                type="button"
+                                onClick={() => setPartnerGender('female')}
+                                className={`w-full py-2 px-4 rounded-md ${partnerGender === 'female' ? 'bg-pink-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+                            >
+                                Женский
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">Возраст партнера:</label>
+                        <div className="flex flex-wrap gap-2">
+                            {ageRanges.map((range) => (
+                                <button
+                                    key={range.value}
+                                    type="button"
+                                    onClick={() => setPartnerAgeRange(range.value)}
+                                    className={`w-full py-2 px-4 rounded-md ${partnerAgeRange === range.value ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+                                >
+                                    {range.label}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="partnerGender">Пол партнера:</label>
-                    <select
-                        id="partnerGender"
-                        value={partnerGender}
-                        onChange={(e) => setPartnerGender(e.target.value)}
-                        className="w-full p-2 border border-gray-300 rounded-md"
-                    >
-                        <option value="">Выберите пол партнера</option>
-                        <option value="male">Мужской</option>
-                        <option value="female">Женский</option>
-                    </select>
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="partnerAgeRange">Возраст партнера:</label>
-                    <select
-                        id="partnerAgeRange"
-                        value={partnerAgeRange}
-                        onChange={(e) => setPartnerAgeRange(e.target.value)}
-                        className="w-full p-2 border border-gray-300 rounded-md"
-                    >
-                        <option value="">Выберите возраст партнера</option>
-                        {ageRanges.map((range) => (
-                            <option key={range.value} value={range.value}>{range.label}</option>
-                        ))}
-                    </select>
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="interestsRange">Ваши интересы:</label>
-                    <select
-                        id="interestsRange"
-                        value={selectedInterest}
-                        onChange={(e) => setSelectedInterest(e.target.value)}
-                        className="w-full p-2 border border-gray-300 rounded-md"
-                    >
-                        <option value="">Выберите интерес</option>
-                        {interestsOptions.map((interest) => (
-                            <option key={interest} value={interest}>{interest}</option>
-                        ))}
-                    </select>
-                </div>
+
                 <button
                     type="submit"
-                    className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline col-span-2"
                 >
                     Начать поиск
                 </button>
